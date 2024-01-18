fee24c549fcae020b61d6221bfedf67beb368de7
Kacperaan <popeqkacper@gmail.com>
2024-01-18 22:41:49 +0100

    Created rhaddon.h

---
+++ include/rhaddon.h
@@ -0,0 +1,13 @@
+#ifdef __cplusplus
+extern "C"
+#endif
+#ifndef RHADDON_H
+#define RHADDON_H
+#include "alias.h"
+#include "error.h"
+#include "optimizer.h"
+#include "token.h"
+#endif
+#ifdef __cplusplus
+}
+#endif
