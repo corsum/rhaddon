9336e5eb6a72bb8d26e599c1e879ff7b623dda8d
Kacperaan <popeqkacper@gmail.com>
2024-01-31 13:59:16 +0100

    Small changes

--- src/main.c
+++ src/main.c
@@ -2,14 +2,11 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    if (argc > 1 && strcmp(argv[1], "--cm") == 0) {
-     printf("FILE: %s\n", argv[2]);
-    }
-    else if (argc > 1 && strcmp(argv[1], "--r") == 0) {
-     printf("RUNNING: %s\n", argv[2]);
-    }
-    else if (argc > 1 && strcmp(argv[1], "--i") == 0){
-     printf("INFO\n");
-    }
+  if(argc > 1 && strcmp(argv[1], "--help")){
+      printf("INFO\n");
+       }
+   else {
+      printf("COMPILING\n");
+      }
     return 0;
 }
