a7b6abd54ea9ca64a3b023d2b2ce0090c685077b
Kacperaan <popeqkacper@gmail.com>
2024-03-28 19:16:53 +0100

    Small changes

--- include/token.h
+++ include/token.h
@@ -151,7 +151,7 @@ char *doubleChars(Token token1, const char *token){
    if(token[0] == '!' ){
      switch(token[1]){
       case '=':
-        printf("minus");
+        DOP.sChar = 3;
    }
   }
  }
